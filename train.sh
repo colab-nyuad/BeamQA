89be9def9894135398c7f8d0e02fad294d067d5c
FarahFif <ef_atif@esi.dz>
2022-12-19 16:01:15 +0400

    added requirements

--- train.sh
+++ train.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 cd MetaQA
-python main.py --hops 2 --kg_type full --mode train-BeamQA --batch_size 128 --do_batchnorm True --do_dropout True --embedding_dim 400 --lr 0.005 --validate_every 5 --epochs 10
+python main.py --hops 3 --kg_type half --mode train-BeamQA --batch_size 128 --do_batchnorm True --do_dropout True --embedding_dim 400 --lr 0.005 --validate_every 10 --epochs 10 --gpu 2
+
+#cd WQSP
+#python main.py --hops 2 --kg_type half --mode train-BeamQA --batch_size 128 --do_batchnorm True --do_dropout True --embedding_dim 400 --lr 0.005 --validate_every 10 --epochs 10 --gpu 1
